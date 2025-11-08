70f80f75e35303d0fdc1c94f8d9a878e6377c42e
SebastianAyala13 <sebasayala0113@gmail.com>
2025-11-07 19:13:02 -0500

    chore: Add Meta Pixel tracking script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -246,7 +246,32 @@ export default function RootLayout({
                  {/* Preconnect to external domains for performance */}
          <link rel="preconnect" href="https://fonts.googleapis.com" />
          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-         
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '24742952045389128');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=24742952045389128&ev=PageView&noscript=1"
+          />
+        </noscript>
+        
                    {/* TrustedForm Lead Tracking - Professional Implementation */}
           <script
             dangerouslySetInnerHTML={{
